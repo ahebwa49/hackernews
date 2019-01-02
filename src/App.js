bfd3d1ffeeb29d20a0c41647ee2a6b0db8f3d9bd
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-02 14:12:39 +0300

    add an array of list items

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import './App.css';
 
 class App extends Component {
   render() {
-    
-    };
+    const list = [
+      {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      },
+    ];
+  };
     return (
       <div className="App">
         <h2>{helloWorld.text}</h2>
