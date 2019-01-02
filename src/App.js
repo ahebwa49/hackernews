2c277196aea0eb6e550166bebcc9317d884b9e5d
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-02 17:37:05 +0300

    substitute the ES6 arrow function with a concise ES6 arrow function

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ class App extends Component {
     ];
     return (
       <div className="App">
-      {list.map((item) => {
-        return(
+      {list.map((item) =>
+
           <div key={item.objectID}>
           <span>
             <a href={item.url}>{item.title}</a>
@@ -34,8 +34,7 @@ class App extends Component {
           <span>{item.num_comments}</span>
           <span>{item.points}</span>
           </div>
-        );
-      })}
+        )}
     </div>
     );
   }
