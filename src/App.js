7f50e8267b10fcb11e249e0ae2baa0ec864cd0fc
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-05 20:14:30 +0300

    fix a simple bug

--- src/App.js
+++ src/App.js
@@ -71,25 +71,27 @@ class Table extends Component {
   render() {
     const { list, pattern, onDismiss } = this.props;
     return (
-      {list.filter(isSearched(pattern)).map(item =>
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
 
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button
-              onClick={() => onDismiss(item.objectID)}
-              type="button"
-            >
-            Dismiss
-            </button>
-          </span>
-        </div>
-        )
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
+              Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+      </div>
     );
   }
 }
