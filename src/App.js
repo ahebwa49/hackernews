5a73de2661d0a5c6b6e85da2347fe9cf34f1a0fe
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-06 08:52:33 +0300

    add a reusable button component

--- src/App.js
+++ src/App.js
@@ -103,5 +103,23 @@ class Table extends Component {
     );
   }
 }
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children,
+    } = this.props;
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+      {children}
+      </button>
+    );
+  }
+}
 
 export default App;
