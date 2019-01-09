6202a75d89c96033333e0174ac35f1c51d471d58
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-09 10:02:46 +0300

    use template literals for string concatenation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const PATH_BASE =  'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
 const list = [
   {
     title: 'React',
