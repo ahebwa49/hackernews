d11eddcf7cf566675e92ffbbf3e3ef12e29408c7
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-09 11:19:16 +0300

    add conditional rendering for no result

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ class App extends Component {
 
   render(){
     const { searchTerm, result } = this.state;
+    if (!result) {return null; }
     return (
       <div className="page">
         <div className = "interactions">
