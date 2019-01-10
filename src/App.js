c5de4c8289a0fd63050c1912477ea7475c78ebfc
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-10 09:47:16 +0300

    add the result to state

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ class App extends Component {
   fetchSearchTopStories(searchTerm){
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
       .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
   }
 
   onSearchChange(event){
