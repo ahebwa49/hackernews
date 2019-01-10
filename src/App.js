d395b90b2fac101114620e3d78be09c07389ace6
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-10 08:46:23 +0300

    use the logical && operator

--- src/App.js
+++ src/App.js
@@ -74,13 +74,12 @@ class App extends Component {
           Search
           </Search>
         </div>
-        {result
-        ? <Table
+        {result &&
+        <Table
           list = {result.hits}
           pattern = {searchTerm}
           onDismiss = {this.onDismiss}
         />
-        : null
       }
     </div>
     );
