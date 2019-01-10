50b32ad16afe0512d8c3f1b598aa13f052b41295
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-10 12:09:12 +0300

    add a page variable

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const DEFAULT_QUERY = 'redux';
 const PATH_BASE =  'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
 
 const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
