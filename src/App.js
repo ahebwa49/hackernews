da1df86bbc48e320bf6702b58147b785ed2d149b
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-11 11:54:35 +0300

    add a composable path constant

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const PATH_BASE =  'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
+const DEFAULT_HPP = 100;
 
 const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 
