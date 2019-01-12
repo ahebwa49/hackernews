7b4b09e88d802c7502350e01efa197aa376e38fd
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-12 11:50:38 +0300

    abort the request

--- src/App.js
+++ src/App.js
@@ -109,8 +109,8 @@ class App extends Component {
   fetchSearchTopStories(searchTerm, page = 0){
     axios.get(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       //.then(response => response.json())
-      .then(result => this.setSearchTopStories(result.data))
-      .catch(error => this.setState({ error }));
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
   }
 
   onSearchChange(event){
